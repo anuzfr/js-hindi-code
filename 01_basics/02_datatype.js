1b089338afc516ec605bfacd8c8e1370c338bed5
anuzfr <156520669+anuzfr@users.noreply.github.com>
2024-01-16 13:30:41 +0000

    datatypes

---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,25 @@
+"use strict"  // treat all js code as newer version 
+//alert (3+3) // we are using nodejs not the browser
+
+console.log(3 
+    + 3)
+
+console.log("hitesh")
+
+
+let name = "anuj"
+let age = 20
+let isLoggedIn = false
+
+// (let) data type for defining the variable
+//bigint
+//string => " "
+//boolean => true / false 
+//null => standalone value 
+//undefined => thats value is undefined 
+//symbol => unique
+//object
+
+console.log(typeof age)
+console.log(typeof undefined) // o/p is undefined 
+console.log(typeof null) // o/p is object => it is a value in it is null
