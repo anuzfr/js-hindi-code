07dfcc14f1cfbdafd41e88b95ec4109593303c11
anuzfr <156520669+anuzfr@users.noreply.github.com>
2024-01-16 12:52:10 +0000

    varible file change

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const acc_id = 1001
+let acc_email = "anuj@anuj.com"
+var acc_pass = "1234"
+acc_city = "delhi"
+let acc_state;
+
+// acc_id = 2000 // no change in constant 
+
+acc_email = "anuj@a.com"
+acc_pass = "4321"
+acc_city = "j&k"
+
+console.log(acc_id);
+console.table([acc_id , acc_email , acc_pass , acc_city , acc_state])
