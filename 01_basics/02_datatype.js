6c3f806bb54cfdd806c09f518b49c55d4ed9510a
anuzfr <156520669+anuzfr@users.noreply.github.com>
2024-01-22 18:59:17 +0000

    data types and types

--- 01_basics/02_datatype.js
+++ 01_basics/02_datatype.js
@@ -23,3 +23,43 @@ let isLoggedIn = false
 console.log(typeof age)
 console.log(typeof undefined) // o/p is undefined 
 console.log(typeof null) // o/p is object => it is a value in it is null
+
+
+
+
+//****************************** data type summary *****************************************
+
+// primitive data type
+ 
+// 7 types    ==> string , number , boolean , null , undefined , symbol , bigInt 
+
+const score = 100 
+const score_value = 100.3
+
+const isLoggedIn = true;
+const outsideTemp = null;
+let userEmail;
+
+const id = symbol("1234");
+const otherId = symbol("1234");
+console.log(id == otherId);
+
+
+const bigNumber = 12345678900987654321n  //bigInt
+
+
+// non primitive data type (reference)
+// 3 types arrays , objects , functions 
+
+let arraySuperhero = ["ironman" , "batman" , "superman"];  // array 
+let objIronman = {
+    name_ironman = tony stark 
+    age = 36
+
+};
+const myFunc = function()
+{
+          console.log("hello world");
+}
+
+console.log(typeof myFunc) 
