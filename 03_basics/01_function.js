0f09896f8e6269dbe1fc3c7c10dd4bf01c4c92a7
anuzfr <156520669+anuzfr@users.noreply.github.com>
2024-01-28 18:46:06 +0000

    function and parameter

---
+++ 03_basics/01_function.js
@@ -0,0 +1,10 @@
+function sayMyName(){
+    console.log("a");
+    console.log("n");
+    console.log("u");
+    console.log("j");
+}
+
+//sayMyName   ==> it is reference 
+//sayMyName() ==> it is execution 
+sayMyName()
