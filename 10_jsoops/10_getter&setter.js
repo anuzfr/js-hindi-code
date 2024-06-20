6707be68cc64ba653331f61e1cd7e2cad0d168b9
anuzfr <156520669+anuzfr@users.noreply.github.com>
2024-06-20 16:38:30 +0000

    getter and setter (properties of object)

---
+++ 10_jsoops/10_getter&setter.js
@@ -0,0 +1,24 @@
+class User {
+    constructor(email, password){
+        this.email = email;
+        this.password = password
+    }
+
+    get email(){
+        return this._email.toUpperCase()   // at the time of getting the email by an user , changes it to upper case 
+    }
+    set email(value){
+        this._email = value              // set the email in db as same as given , which is lower case as initial 
+    }
+
+    get password(){
+        return `${this._password}lol`     // user getting email with the lol word 
+    }
+
+    set password(value){
+        this._password = value           // but set as only as given = anuj 
+    }
+}
+
+const anuj = new User("a@anuj.com", "anuj")
+console.log(anuj.email);
